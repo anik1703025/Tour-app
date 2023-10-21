a743efe52b06279747828ebd2397e27aaeada7c2
anik1703025 <106774838+anik1703025@users.noreply.github.com>
2023-10-21 19:21:19 +0600

    Your commit message here

---
+++ lib/main.dart
@@ -0,0 +1,89 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:shelter/const/app_colors.dart';
+import 'package:shelter/const/app_strings.dart';
+import 'package:shelter/ui/route/route.dart';
+import 'package:shelter/ui/views/splash_screen.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await GetStorage.init();
+  runApp(
+    App(),
+  );
+}
+
+class App extends StatelessWidget {
+  // Create the initialization Future outside of `build`:
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp(
+    name: "Shelter",
+    options: FirebaseOptions(
+        apiKey: "AIzaSyAczTIarKMd7QEgvXF3rTahRNHveoKcVWI",
+        appId: "1:344176130966:android:4edab248f189bca40ebbb7",
+        messagingSenderId: "344176130966",
+        projectId: "shelter-b9151"),
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      // Initialize FlutterFire:
+      future: _initialization,
+      builder: (context, snapshot) {
+        // Check for errors
+        if (snapshot.hasError) {
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+
+        // Once complete, show your application
+        if (snapshot.connectionState == ConnectionState.done) {
+          return MyApp();
+        }
+
+        // Otherwise, show something whilst waiting for initialization to complete
+        return Center(
+          child: CircularProgressIndicator(),
+        );
+      },
+    );
+  }
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return ScreenUtilInit(
+      designSize: Size(428, 926),
+      builder: (_, child) {
+        return GetMaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: AppStrings.appName,
+          theme: ThemeData(
+              appBarTheme: AppBarTheme(
+                titleTextStyle: TextStyle(color: Colors.black),
+                iconTheme: IconThemeData(
+                  color: Colors.black,
+                ),
+              ),
+              primarySwatch: Colors.blue,
+              textTheme: GoogleFonts.poppinsTextTheme(
+                Theme.of(context).textTheme.apply(),
+              ),
+              scaffoldBackgroundColor: AppColors.scaffoldColor),
+          initialRoute: splash,
+          getPages: getPages,
+          home: SplashScreen(),
+        );
+      },
+    );
+  }
+}
---
+++ lib/ui/views/drawer_pages/profile.dart
@@ -0,0 +1,111 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/get.dart';
+import 'package:toggle_switch/toggle_switch.dart';
+
+import '../../../business_logics/form.dart';
+import '../../../const/app_colors.dart';
+import '../../styles/styles.dart';
+import '../../widgets/violetButton.dart';
+
+class UserProfile extends StatelessWidget {
+  TextEditingController _nameController = TextEditingController();
+  TextEditingController _phoneController = TextEditingController();
+  TextEditingController _addressController = TextEditingController();
+  Rx<TextEditingController> _dobController = TextEditingController().obs;
+  String? dob;
+  String gender = "Male";
+  Rx<DateTime> selectedDate = DateTime.now().obs;
+
+  _selectDate(BuildContext context) async {
+    final selected = await showDatePicker(
+      context: context,
+      initialDate: selectedDate.value,
+      firstDate: DateTime(2000),
+      lastDate: DateTime(2025),
+    );
+
+    if (selected != null && selected != selectedDate) {
+      dob = "${selected.day} - ${selected.month} - ${selected.year}";
+      _dobController.value.text = dob!;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
+        title: Text("Profile"),
+      ),
+      body: Padding(
+        padding: EdgeInsets.only(left: 30.w, right: 30.w, top: 20.h),
+        child: SingleChildScrollView(
+          scrollDirection: Axis.vertical,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              FormField(_nameController, TextInputType.name, 'name'),
+              FormField(_phoneController, TextInputType.number, 'number'),
+              FormField(_addressController, TextInputType.text, 'address'),
+              Obx(
+                () => TextFormField(
+                  controller: _dobController.value,
+                  readOnly: true,
+                  decoration: InputDecoration(
+                    hintText: "date of birth",
+                    hintStyle: TextStyle(
+                      fontSize: 15.sp,
+                    ),
+                    suffixIcon: IconButton(
+                      onPressed: () => _selectDate(context),
+                      icon: Icon(Icons.calendar_month_rounded),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 10.h,
+              ),
+              ToggleSwitch(
+                initialLabelIndex: 0,
+                totalSwitches: 2,
+                labels: [
+                  'Male',
+                  'Female',
+                ],
+                onToggle: (index) {
+                  if (index == 0) {
+                    gender = "Male";
+                  } else {
+                    gender = "Female";
+                  }
+                  print('switched to: $index');
+                },
+              ),
+              SizedBox(
+                height: 20.h,
+              ),
+              VioletButton(
+                "Update",
+                () {},
+              ),
+              
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+Widget FormField(controller, inputType, hint) {
+  return TextFormField(
+    controller: controller,
+    keyboardType: inputType,
+    decoration: AppStyles().textFieldDecoration(hint),
+  );
+}
